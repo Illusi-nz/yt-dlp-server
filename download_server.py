1b7e4ca9a60dcaf53d79a2d7c1d8328f9dabd68d
Illusionz <william@schnell.nu>
2025-07-31 23:21:43 +0200

    fixed some shit

--- download_server.py
+++ download_server.py
@@ -2,8 +2,10 @@ from flask import Flask, request, send_file
 import yt_dlp
 import os
 import uuid
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/download')
 def download():
